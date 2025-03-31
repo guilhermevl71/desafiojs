4795f9a0699d61d23210951903568913de23f96b
Guilherme Vilã <guilhermesnts.profissional@gmail.com>
2025-03-31 15:19:35 -0300

    formulario feito

--- js/form.js
+++ js/form.js
@@ -2,7 +2,14 @@
 //class contato
 
 class contato {
-    
+    constructor(nome, sobrenome, email, cpf, telefone, contato) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.contato = contato;
+    }
 }
 
 function Post(form) {
@@ -13,6 +20,10 @@ function Post(form) {
             form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
             form.elements.namedItem("contato").value);
+
+        let dados = JSON.stringify(data); // let dados é igual aos dados do formulario em json
+        localStorage.setItem("formulario", dados); // o set item salva os dados no localstorage do navegador, formulario é o nome da "variavel" onde está saldo, e dados oq sera salvo
+        
   
 }
 
@@ -23,5 +34,7 @@ function Enviar() {
     if (nome.value != "") {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
     }
-
+    
+    let dadosform = localStorage.getItem("formulario");
+    console.log(dadosform);
 }
